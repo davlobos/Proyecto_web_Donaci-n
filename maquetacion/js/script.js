fa37fc8483a99acc56d174f9d415aa332885cad8
Sashiiii <camila.barrera.a@mail.pucv.cl>
2024-04-16 13:51:08 -0400

    cambiar de pestaña

--- maquetacion/js/script.js
+++ maquetacion/js/script.js
@@ -1,8 +1,40 @@
 const navTabs = document.querySelectorAll('.btabbar-item');
+const navContent = document.querySelectorAll('.items');
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the element with class "start"
+    var startElement = document.querySelector(".start");
+    // Make the element visible
+    startElement.style.visibility = "visible";
+});
 
 navTabs.forEach(navTab => {
     navTab.addEventListener('click', () => {
+        console.log(navTab.id);
         document.querySelector('.active')?.classList.remove('active');
         navTab.classList.add('active');
+
+        var items = document.getElementsByClassName("items");
+        for (var i = 0; i < items.length; i++) {
+                items[i].style.visibility = "hidden";            
+        }
+        for (var i = 0; i < items.length; i++) {
+            if (navContent[i].classList.contains(navTab.id)){
+                items[i].style.visibility = "visible";
+            }
+        }
+
+        if(navTab.id == "start"){
+            titulo = "Inicio";
+        }else if (navTab.id == "saved"){
+            titulo = "Favoritos";
+        }else if( navTab.id == "account"){
+            titulo = "Cuenta";
+        }else if(navTab.id == "register"){
+            titulo = "Registrarse";
+        }
+        document.getElementById("id01").innerHTML = titulo;
     });
 });
+
