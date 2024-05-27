117d6533d6886038dc3255d43f0bb7cd35ad1978
davlobos <carlos.dasilva.v@mail.pucv.cl>
2024-05-26 21:55:46 -0400

    arreglos de diseño de app

--- Entrega2/proyecto/src/pages/Campaign.tsx
+++ Entrega2/proyecto/src/pages/Campaign.tsx
@@ -1,4 +1,4 @@
-import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonBackButton, IonButtons, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonProgressBar } from '@ionic/react';
 import { useParams } from 'react-router';
 import './Campaign.css';
 import data from './campaigns.json';
@@ -10,13 +10,16 @@ interface CampaignParams {
 const Campaign: React.FC = () => {
     const { id } = useParams<CampaignParams>();
     const campaign = data.find(c => c.id === parseInt(id));
+    const donatedAmount = 5;
+    const goalAmount = 10;
+    const donationPercentage = donatedAmount / goalAmount;
 
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar>
                     <IonButtons slot='start'>
-                    <IonBackButton defaultHref='/Campaign' />
+                        <IonBackButton defaultHref='/Campaign' />
                     </IonButtons>
                     <IonTitle>Campaña</IonTitle>
                 </IonToolbar>
@@ -29,6 +32,11 @@ const Campaign: React.FC = () => {
                         <h2 className="campaign-title">{campaign.title}</h2>
                         <p>{campaign.subtitle}</p>
                         <img className="campaign-image" src={campaign.imgURL} alt={campaign.title} />
+                        <div className="donation-container">
+                            <IonProgressBar value={donationPercentage} color="success"></IonProgressBar>
+                            <p>$$ donados de $$$ meta</p>
+                            <IonButton color="primary">Dona</IonButton>
+                        </div>
                     </div>
                 ) : (
                     <p>Campaign not found</p>
@@ -39,4 +47,3 @@ const Campaign: React.FC = () => {
 };
 
 export default Campaign;
-
