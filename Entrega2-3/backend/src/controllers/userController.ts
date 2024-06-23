d024682070070eb6917b7be6cbb8fe1a943e9517
freksel <alejandro.marin.s@mail.pucv.cl>
2024-06-23 14:14:20 -0400

    Hashear constraseña, desde creación a cambio de constraseña

--- Entrega2-3/backend/src/controllers/userController.ts
+++ Entrega2-3/backend/src/controllers/userController.ts
@@ -13,8 +13,8 @@ export const login = async (req: Request, res: Response) => {
       return res.status(404).json({ message: 'Usuario no encontrado' });
     }
 
-    // Comparar la contraseña ingresada con la contraseña almacenada en la base de datos
-    const passwordMatch = await bcrypt.compare(password, user.password);
+    // Comparar la contraseña ingresada hasheada con la contraseña almacenada hasheada en la base de datos
+    const passwordMatch = await bcrypt.compareSync(password, user.password);
   
     if (!passwordMatch) {
       return res.status(401).json({ message: 'Contraseña incorrecta' });
@@ -38,6 +38,7 @@ export const getUsers = async (req: Request, res: Response) => {
 
 export const createUser = async (req: Request, res: Response) => {
   try {
+    User.password= bcrypt.hashSync(User.password,10);
     const newUser = await userService.createUser(req.body);
     res.status(201).json({data: newUser});
   } catch (error) {
@@ -48,6 +49,7 @@ export const createUser = async (req: Request, res: Response) => {
 export const updateUser = async (req: Request, res: Response) => {
   const { id } = req.params;
   try {
+    User.password= bcrypt.hashSync(User.password,10);
     const updatedUser = await userService.updateUser(id, req.body);
     if (!updatedUser) {
       return res.status(404).json({ message: 'Usuario no encontrado' });
